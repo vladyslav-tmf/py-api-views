6fbadbf1548c627d562717c3dedad434cc793596
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-23 20:31:44 +0200

    Add Cinema Hall view and update urls
    - Implemented "GenericViewSet" view for CinemaHall model.
    - Updated URLs to include paths for cinema hall endpoints.

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,6 +4,18 @@ from cinema import views
 
 app_name = "cinema"
 
+cinema_hall_list = views.CinemaHallViewSet.as_view(
+    actions={"get": "list", "put": "create"}
+)
+cinema_hall_detail = views.CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
+
 urlpatterns = [
     path("genres/", views.GenreList.as_view(), name="genre-list"),
     path(
@@ -11,5 +23,11 @@ urlpatterns = [
         views.GenreDetail.as_view(),
         name="genre-detail"),
     path("actors/", views.ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>/", views.ActorDetail.as_view(), name="actor-detail")
+    path("actors/<int:pk>/", views.ActorDetail.as_view(), name="actor-detail"),
+    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path(
+        "cinema-halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
+    ),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework import generics, mixins, status, viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from cinema.models import Actor, Genre
-from cinema.serializers import ActorSerializer, GenreSerializer
-from rest_framework import generics
+
+from cinema.models import Actor, CinemaHall, Genre
+from cinema.serializers import (
+    ActorSerializer,
+    CinemaHallSerializer,
+    GenreSerializer
+)
 
 
 class GenreList(APIView):
@@ -32,7 +36,6 @@ class GenreDetail(APIView):
         serializer = GenreSerializer(self.get_object(pk=pk))
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
     def put(self, request: Request, pk: int) -> Response:
         serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -41,12 +44,12 @@ class GenreDetail(APIView):
 
     #  Possible, but pointless
     # def patch(self, request: Request, pk: int) -> Response:
-        # serializer = GenreSerializer(
-        #     self.get_object(pk=pk), data=request.data, partial=True
-        # )
-        # serializer.is_valid(raise_exception=True)
-        # serializer.save()
-        # return Response(serializer.data, status=status.HTTP_200_OK)
+    #   serializer = GenreSerializer(
+    #       self.get_object(pk=pk), data=request.data, partial=True
+    #   )
+    #   serializer.is_valid(raise_exception=True)
+    #   serializer.save()
+    #   return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request: Request, pk: int) -> Response:
         self.get_object(pk=pk).delete()
@@ -61,3 +64,15 @@ class ActorList(generics.ListCreateAPIView):
 class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
